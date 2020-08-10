bed13170065df502448ba388ec1c867f0d64c506
Caroline Strickland <carolinelstrickland@outlook.com>
2020-08-10 12:57:47 -0500

    pushing on

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ function displayWeatherInfo() {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(queryURL)
-        console.log(response)
+        let currentName = $("#current-selection").text(response.name);
+        let currentWeather = $("#current-weather")
+        currentWeather.html("");
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        let pOne = $("<p>").text("Temperature: " + tempF.toFixed(2));
+        currentWeather.append(pOne);
+        let pTwo = $("<p>").text("Humidity: " + response.main.humidity);
+        currentWeather.append(pTwo);
+        let pThree = $("<p>").text("Wind Speed: " + response.wind.speed);
+        currentWeather.append(pThree);
     });
-
 }
 
 
@@ -21,7 +28,7 @@ function displayCity() {
     $("#cities-view").empty();
     for (let i=0; i < cities.length; i++) {
         let a = $("<button>");
-        a.addClass("city");
+        a.addClass("city-btn");
         a.addClass("row");
         a.attr("data-name", cities[i]);
         a.text(cities[i]);
@@ -32,11 +39,11 @@ function displayCity() {
 $("#city-input").on("click", function(event) {
     event.preventDefault();
     let city = $("#city-entry").val();
-    console.log(city);
-    cities.push(city);
-    
+    cities.push(city); 
     displayCity();
 });
 
-$(document).on("click", ".city", displayWeatherInfo);
+$(document).on("click", ".city-btn", displayWeatherInfo);
+
+displayCity();
 
